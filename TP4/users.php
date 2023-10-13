760e12f1c3daaec4f09ea552f3e95183b7acc3f2
m-spi <matis.spinelli30@gmail.com>
2023-10-13 18:20:23 +0200

    TP4-2

--- TP4/users.php
+++ TP4/users.php
@@ -1,15 +1,46 @@
 <?php
   require_once('open.php');
 
-  $request = $pdo->prepare("select * from users");
+  if(isset($_POST['add']) && strlen($_POST['name']) > 0 && strlen($_POST['name']) > 0){
+    try{
+      $request = $pdo->prepare("INSERT INTO users (id, name, email) VALUES (NULL, '{$_POST['name']}', '{$_POST['email']}')");
+      $request->execute();
+    }catch(PDOException $erreur){
+      echo 'Erreur : '.$erreur->getMessage();
+    }
+  }elseif(isset($_POST['change']) && strlen($_POST['name']) > 0 && strlen($_POST['name']) > 0){
+    $once = 0;
+    foreach($_POST as $key => $value){
+      if(is_numeric($key)){
+        $once++;
+        $id = $key;
+      }
+    }
+    if($once == 1){
+      try{
+        $request = $pdo->prepare("UPDATE users SET name='{$_POST['name']}', email='{$_POST['email']}' WHERE id={$id}");
+        $request->execute();
+      }catch(PDOException $erreur){
+        echo 'Erreur : '.$erreur->getMessage();
+      }
+    }
+  }elseif (isset($_POST['delete'])) {
+    foreach ($_POST as $key => $value) {
+      if(is_numeric($key)){
+        try{
+          $request = $pdo->prepare("DELETE FROM users WHERE id={$key}");
+          $request->execute();
+        }catch(PDOException $erreur){
+          echo 'Erreur : '.$erreur->getMessage();
+        }
+      }
+    }
+  }
 
-  // TODO: add your code here
-  // retrieve data from database using fetch(PDO::FETCH_OBJ) and
-  // display them in HTML array
+  $request = $pdo->prepare("SELECT * FROM users");
 
   $request->execute();
   $res = $request->fetchAll(PDO::FETCH_OBJ);
-
 ?>
 
 <!DOCTYPE html>
@@ -21,38 +52,39 @@
   <body>
     <h1>Users</h1>
     <hr>
-    <table>
-      <tr>
-        <th>Id</th>
-        <th>Nom</th>
-        <th>Email</th>
-      </tr>
-      <?php
-        foreach($res as $r){
-          echo "<tr>
-                  <td>{$r->id}</td>
-                  <td>{$r->name}</td>
-                  <td>{$r->email}</td>
-                </tr>
-          ";}
-      ?>
-    </table>
-    <hr>
-    <form action="adduser.php" method="post">
+    <form method="post">
+      <table>
+        <tr>
+          <th></th>
+          <th>Id</th>
+          <th>Nom</th>
+          <th>Email</th>
+        </tr>
+        <?php
+          foreach($res as $r){
+            echo "<tr>
+                    <td><input type=\"checkbox\" name=\"{$r->id}\"/></td>
+                    <td>{$r->id}</td>
+                    <td>{$r->name}</td>
+                    <td>{$r->email}</td>
+                  </tr>
+            ";}
+        ?>
+      </table>
+      <hr>
       <table>
         <tr>
           <th>Nom :</th>
-          <td><input type="text" name="name"></td>
+          <td><input type="text" name="name"/></td>
         </tr>
         <tr>
           <th>Email :</th>
-          <td><input type="text" name="email"></td>
-        </tr>
-        <tr>
-          <th></th>
-          <td><input type="submit" value="Créer utilisateur" /></td>
+          <td><input type="text" name="email"/></td>
         </tr>
       </table>
+      <input type="submit" name="add" value="Créer utilisateur"/>
+      <input type="submit" name="change" value="Changer l'utilisateur"/>
+      <input type="submit" name="delete" value="Supprimer"/>
     </form>
   </body>
 </html>
